978bf8bd73503ccfdf6e13b659f6bd2b0979c2be
DoctorPatita <thefoxyparty78@gmail.com>
2025-11-07 21:09:16 -0300

    OCR con API de Google

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const pool = require('./db'); // Importar la conexión
+const multer = require('multer');
+const path = require('path');
 const app = express();
 const port = 3000;
+
+//Importamos la funcion quickstart pal OCR
+const { quickstart } = require('./OCR/ocr.js');
+//config de multer para almacenamiento en RAM
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 // Ruta de prueba que guarda un mensaje en la base de datos
 app.get('/save', async (req, res) => {
@@ -29,6 +37,35 @@ app.get('/messages', async (req, res) => {
 app.get('/', (req, res) => {
   res.send('¡Bienvenido! Usa /save para guardar un mensaje y /messages para verlos.');
 });
+
+app.get('/ocr', (req, res) => {
+    res.sendFile(path.join(__dirname, 'OCR', 'formulario.html'));
+});
+
+//Para manejar la subida del archivo
+app.post('/upload', upload.single('imagenCedula'), async (req, res) => {
+    console.log('Archivo recibido en memoria.');
+
+    if (!req.file) {
+      return res.status(400).send('No se ha subido ningún archivo.');
+    }
+
+    try {
+        const fileBuffer = req.file.buffer;
+        const mimeType = req.file.mimetype;
+        const encodedImage = fileBuffer.toString('base64');
+        
+        //Llamamos la funcion del OCR
+        const datosExtraidos = await quickstart(encodedImage, mimeType);
+        res.status(200).json({ 
+          mensaje: 'Imagen procesada correctamente',
+          resultado: datosExtraidos
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'No se pudo procesar la imagen en el servidor.' });
+    }
+});
+
 
 app.listen(port, () => {
   console.log(`App corriendo en http://localhost:${port}`);
